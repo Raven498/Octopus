ecd7feae3435c8e6b40962c866bbe8458a55f2cc
Raven498 <78182016+Raven498@users.noreply.github.com>
2023-03-02 19:44:46 -0500

    Globalized indexing for parser

--- Parser.java
+++ Parser.java
@@ -4,26 +4,7 @@ import java.util.Map;
 public class Parser{
     private ArrayList<String> code;
     private final String[] filter = {":", "(", ")", ","};
-    private final InstanceParseTree instPT = new InstanceParseTree();
-    private final ModelParseTree modelPT = new ModelParseTree();
-    private final ArrayList<String>[] mpt_lists = [modelPT.DefMembers,
-                                                modelPT.AI, 
-                                                modelPT.A_types, 
-                                                modelPT.A_access, 
-                                                modelPT.BI, 
-                                                modelPT.B_returns,
-                                                modelPT.B_params,
-                                                modelPT.B_paramtypes,
-                                                modelPT.B_access];
-    private final ArrayList<String>[] ipt_lists = [instPT.In_Members
-                                                instPT.AI, 
-                                                instPT.A_types, 
-                                                instPT.A_access, 
-                                                instPT.BI, 
-                                                instPT.B_returns,
-                                                instPT.B_params,
-                                                instPT.B_paramtypes,
-                                                instPT.B_access];
+    private int cursor = 0;
     private Map<String, String> keys = Map.of(
         "Model-Key", "new",
         "Model-B-Key", "-",
@@ -42,32 +23,60 @@ public class Parser{
         return word;
     }
 
-    public void MSP(int index){
+    public ModelParseTree MSP(){
+        final ModelParseTree modelPT = new ModelParseTree();
+        final ArrayList<String>[] mpt_lists = [modelPT.DefMembers,
+                                                modelPT.AI, 
+                                                modelPT.A_types, 
+                                                modelPT.A_access, 
+                                                modelPT.BI, 
+                                                modelPT.B_returns,
+                                                modelPT.B_params,
+                                                modelPT.B_paramtypes,
+                                                modelPT.B_access];
         modelPT.MI = filter(code[index]);
-        index += 2;
+        cursor += 2;
         modelPT.DefMembers.add(filter(code.get(index)));
-        index += 2;
+        cursor += 2;
         
-        int tree_index = 1;
-        for(int i = index; i < code.size(); i++){
-            mpt_lists[tree_index].add(code.get(i).replace(",", ""));
+        int tree_index = 0;
+        for(cursor; cursor < code.size(); cursor++){
+            if(code.get(cursor).)
+            mpt_lists[tree_index].add(code.get(cursor).replace(",", ""));
+            cursor++;
+            tree_index++;
         }
-
-        //TODO: Return parse trees to parse()
     }
 
-    public void ISP(int index){
-        
+    public InstanceParseTree ISP(){
+        final InstanceParseTree instPT = new InstanceParseTree();
+        final ArrayList<String>[] ipt_lists = [instPT.In_Members
+                                                instPT.AI, 
+                                                instPT.A_types, 
+                                                instPT.A_access, 
+                                                instPT.BI, 
+                                                instPT.B_returns,
+                                                instPT.B_params,
+                                                instPT.B_paramtypes,
+                                                instPT.B_access];
+
+
     }
 
     public void parse(){
-        for(int i = 0; i < code.size(); i++){
-            if(word.equals(keys.get("Model-Key"))){
-                MSP(i + 1);
-                //TODO: Return parse trees to parse()
+        ArrayList<ModelParseTree> MPTs = new ArrayList<>();
+        ArrayList<InstanceParseTree> IPTs = new ArrayList<>();
+        for(cursor = 0; cursor < code.size(); cursor++){
+            if(code.get(cursor).equals(keys.get("Model-Key"))){
+                cursor += 1;
+                ModelParseTree mpt = MSP();
+                MPTs.add(mpt);
             }
             else if(word.equals(keys.get("In-Key"))){
-                ISP(i + 1);
+                cursor += 1;
+                ISP();
+                ModelParseTree mpt = MSP();
+                MPTs.add(mpt);
             }
         }
     }
